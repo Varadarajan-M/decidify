7694a02a9270e8beed3c4acbf4fd4c3e72409c8a
Varadarajan M <varad2k12@gmail.com>
2023-05-13 17:40:07 +0530

    Completed landing page UI

--- decidify-app/src/app/page.tsx
+++ decidify-app/src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from 'next/image';
+import '@/styles/pages/landing-page.scss';
 
-export default function Home() {
-	return <main>Hello world!</main>;
+export default function LandingPage() {
+	return (
+		<main className='container'>
+			<h1 className='gradient-header'>Decidify!</h1>
+			<p className='tagline-text'>
+				Stop second-guessing and start deciding! Create your own
+				<b> FREE anonymous</b> polls and let the people decide!
+			</p>
+			<button className='create-poll-btn'>Let&apos;s create a poll</button>
+		</main>
+	);
 }
