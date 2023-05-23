b90ec09e6736d33f6abfd88cec10d83bfc3f9350
Varadarajan M <varad2k12@gmail.com>
2023-05-24 01:17:45 +0530

    Completed Poll Feature

---
+++ decidify-app/src/app/poll/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import '@/styles/pages/poll.scss';
+import { Suspense } from 'react';
+import PollContainer from './PollContainer';
+type PageProps = {
+	params: {
+		slug: string;
+	};
+};
+
+const fakeApi = async () => {
+	return new Promise<any>((resolve) =>
+		resolve({
+			Poll_Question: 'What to eat today?',
+			Poll_Options: [
+				'Pizza',
+				'Burger',
+				'Nachos',
+				'Sizzlers',
+				'Briyani',
+				'Tacos',
+			],
+		}),
+	);
+};
+
+const PollPage = async ({ params }: PageProps) => {
+	const { slug } = params;
+	const pollDetails = await fakeApi();
+	return (
+		<section className='container'>
+			<Suspense fallback={<h1>Page loading..</h1>}>
+				<PollContainer pollDetails={pollDetails} />
+			</Suspense>
+		</section>
+	);
+};
+
+export default PollPage;
